efea594c8c8cbb2319dad62908245f74f33a4946
Varun Kumar <varungecsiwan@gmail.com>
2026-04-27 22:28:08 +0530

    Setup project structure and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,7 @@
-import { useState } from "react";
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>🚀 My React App</h1>
-      <p>Welcome Varun 👋</p>
-
-      <h2>Counter: {count}</h2>
-
-      <button onClick={() => setCount(count + 1)}>
-        Increment ➕
-      </button>
-
-      <button onClick={() => setCount(count - 1)} style={{ marginLeft: "10px" }}>
-        Decrement ➖
-      </button>
-    </div>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
